de70653b15c409e7c0c613320103e67e7be798d7
christoffer0001 <chbe0001@edu.mercantec.dk>
2026-04-22 10:14:02 +0200

    Map with heatmap overlay has been created, though using sample data with sample locations.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,44 @@
+let testData;
+let cfg;
+
 function setup() {
-  createCanvas(800, 720);
+  // Heatmap config
+  cfg = {
+    radius: 25,
+    maxOpacity: 1,
+    minOpacity: 0.5,
+    blur: 0.75,
+    scaleRadius: false,
+    useLocalExtrema: true,
+    latField: "lat",
+    lngField: "lng",
+    valueField: "count",
+  };
+
+  //Sample data ONLY
+  testData = {
+    max: 8,
+    data: [
+      {lat: 56.44879, lng: 9.396084, count: 8},
+      {lat: 56.449, lng: 9.397, count: 6},
+      {lat: 56.447, lng: 9.395, count: 7},
+    ],
+  };
 }
 
 function draw() {
-  background(220);
+  // Create map inside of div (from html)
+  let map = L.map("map-canvas", {
+    center: [56.44879, 9.396084],
+    zoom: 13,
+  });
+
+  // Display base layer
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+  // Heatmap layer
+  let heatmapLayer = new HeatmapOverlay(cfg).addTo(map);
+
+  // Apply data to the heatmap overlay
+  heatmapLayer.setData(testData);
 }
